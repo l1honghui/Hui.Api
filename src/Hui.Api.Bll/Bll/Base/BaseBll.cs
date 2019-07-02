9fff196456f6d29a0d3137f0b8292db774d3641b
宏辉 李 <lihonghui1012@outlook.com>
2019-07-02 10:12:10 +0800

    常用CURD封装

---
+++ src/Hui.Api.Bll/Bll/Base/BaseBll.cs
@@ -0,0 +1,86 @@
+﻿using Hui.Api.Dal.Repositories;
+using Hui.Api.Model.Entity.IEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hui.Api.Bll
+{
+    /// <summary>
+    /// 常用CURD封装
+    /// </summary>
+    /// <typeparam name="TRepository"></typeparam>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TPrimaryKey"></typeparam>
+    public class BaseBll <TRepository, TEntity, TPrimaryKey> :  IBaseBll<TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+         where TRepository : IRepository<TEntity, TPrimaryKey>
+    {
+        private readonly TRepository _repository;
+
+        public BaseBll(TRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            var retEntity = await _repository.AddAsync(entity);
+            await _repository.SaveAsync();
+            return retEntity;
+        }
+
+        public async Task<int> AddRangeAsync(List<TEntity> entityList)
+        {
+            _repository.AddRange(entityList);
+            return await _repository.SaveAsync();
+        }
+
+        public async Task<int> RemoveAsync(TPrimaryKey id)
+        {
+            _repository.Remove(id);
+            return await _repository.SaveAsync();
+        }
+
+        public async Task<int> RemoveAsync(TEntity entity)
+        {
+            _repository.Remove(entity);
+            return await _repository.SaveAsync();
+        }
+
+        public async Task<int> RemoveRangeAsync(List<TEntity> entityList)
+        {
+            _repository.RemoveRange(entityList);
+            return await _repository.SaveAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _repository.Update(entity);
+            await _repository.SaveAsync();
+            return entity;
+        }
+
+        public async Task<int> UpdateRangeAsync(List<TEntity> entityList)
+        {
+            _repository.UpdateRange(entityList);
+            return await _repository.SaveAsync();
+        }
+
+        public async Task<TEntity> GetAsync(TPrimaryKey id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        public async Task<T> QueryAsync<T>(Func<IQueryable<TEntity>, T> expression)
+        {
+            return await _repository.QueryAsync(expression);
+        }
+
+        public async Task<T> QueryAsNoTrackingAsync<T>(Func<IQueryable<TEntity>, T> expression)
+        {
+            return await _repository.QueryAsNoTrackingAsync(expression);
+        }
+    }
+}
---
+++ src/Hui.Api.Bll/Bll/Base/IBaseBll.cs
@@ -0,0 +1,84 @@
+﻿using Hui.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hui.Api.Bll
+{ 
+    public interface IBaseBll<TEntity, TPrimaryKey> : IDependency
+       where TEntity : class
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<TEntity> AddAsync(TEntity entity);
+
+        /// <summary>
+        /// 批量新增
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        Task<int> AddRangeAsync(List<TEntity> entityList);
+
+        /// <summary>
+        /// 删除（根据主键）
+        /// </summary>
+        /// <param name="primaryKey"></param>
+        /// <returns></returns>
+        Task<int> RemoveAsync(TPrimaryKey primaryKey);
+
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<int> RemoveAsync(TEntity entity);
+
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        Task<int> RemoveRangeAsync(List<TEntity> entityList);
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        /// <summary>
+        /// 批量修改
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        Task<int> UpdateRangeAsync(List<TEntity> entityList);
+
+        /// <summary>
+        /// 查询（根据主键）
+        /// </summary>
+        /// <param name="primaryKey"></param>
+        /// <returns></returns>
+       Task<TEntity> GetAsync(TPrimaryKey id);
+
+        /// <summary>
+        /// 查询（自定义查询表达式）
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        Task<T> QueryAsync<T>(Func<IQueryable<TEntity>, T> expression);
+
+        /// <summary>
+        /// 查询（自定义查询表达式）
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        Task<T> QueryAsNoTrackingAsync<T>(Func<IQueryable<TEntity>, T> expression);
+    }
+}

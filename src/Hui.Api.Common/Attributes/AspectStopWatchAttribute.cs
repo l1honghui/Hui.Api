9bc4360264c5b87ea389cd56712513a448d1dfc2
lihonghui <lihonghui1012@outlook.com>
2019-09-03 10:38:15 +0800

    clean code

---
+++ src/Hui.Api.Common/Attributes/AspectStopWatchAttribute.cs
@@ -0,0 +1,41 @@
+﻿using AspectCore.DynamicProxy;
+using Hui.Api.Common.ServiceProvider;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+
+namespace Hui.Api.Common.Attributes
+{
+    /// <summary>
+    /// 接口耗时监控Aop实现
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AspectStopWatchAttribute : AbstractInterceptorAttribute
+    {
+        private const long WarningMilliseconds = 1000;
+
+        private readonly ILogger<AspectStopWatchAttribute> _logger = ServiceProviderHelper.GetService<ILogger<AspectStopWatchAttribute>>();
+
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            await next(context);
+            stopWatch.Stop();
+            var timeElapsed = stopWatch.ElapsedMilliseconds;
+            var methodInfo =
+                $"{context.ServiceMethod.DeclaringType?.Namespace}.{context.ServiceMethod.DeclaringType?.Name}.{context.ServiceMethod.Name}";
+            var logMsg =
+                $"--->>> MethodInfo:{methodInfo},Invoke ElapsedMilliseconds:{timeElapsed};MethodInfo:{methodInfo}";
+            if (timeElapsed > WarningMilliseconds)
+            {
+                _logger.LogWarning(logMsg);
+            }
+            else
+            {
+                _logger.LogInformation(logMsg);
+            }
+        }
+    }
+}
